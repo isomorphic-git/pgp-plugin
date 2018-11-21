d81998608f96ffc7024968dc0c5bb0e814ac6bcb
William Hilton <wmhilton@gmail.com>
2018-11-20 23:54:02 -0500

    fix tests

--- spec/pgp-plugin.spec.js
+++ spec/pgp-plugin.spec.js
@@ -1,6 +1,6 @@
 /* eslint-env node, browser, jasmine */
 const { pgp } = require('..')
-const { secretKey, publicKey } = require('./__fixtures__/keys.json')
+const { secretKey, publicKey } = require('./__helpers__/keys.js')
 
 const cryptoRandomString = require('crypto-random-string')
 
@@ -13,17 +13,25 @@ describe('sign & verify', async () => {
       expect.stringContaining('-----BEGIN PGP SIGNATURE-----')
     )
     let { valid, invalid } = await pgp.verify({ payload, publicKey, signature })
-    expect(valid).toEqual(['a01edd29ac0f3952'])
+    expect(valid).toEqual(['f2f0ced8a52613c4'])
     expect(invalid).toEqual([])
   })
 
-  it('corrupted payload is invalid signature', async () => {
+  it('corrupted payload throws error', async () => {
     let payload = 'Hello world'
     let { signature } = await pgp.sign({ payload, secretKey })
     payload = 'Hello_world'
-    let { valid, invalid } = await pgp.verify({ payload, publicKey, signature })
-    expect(invalid).toEqual(['a01edd29ac0f3952'])
-    expect(valid).toEqual([])
+    let error = null
+    try {
+      let { valid, invalid } = await pgp.verify({
+        payload,
+        publicKey,
+        signature
+      })
+    } catch (err) {
+      error = err
+    }
+    expect(error).not.toBeNull()
   })
 
   it('corrupted signature throws error', async () => {
@@ -52,7 +60,7 @@ describe('sign & verify', async () => {
       expect.stringContaining('-----BEGIN PGP SIGNATURE-----')
     )
     let { valid, invalid } = await pgp.verify({ payload, publicKey, signature })
-    expect(valid).toEqual(['a01edd29ac0f3952'])
+    expect(valid).toEqual(['f2f0ced8a52613c4'])
     expect(invalid).toEqual([])
   })
 
