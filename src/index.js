f6a1ceefe905f7a9507c9189cfbd33ce41f0ecac
William Hilton <wmhilton@gmail.com>
2018-11-21 23:53:41 -0500

    fix: Add browser bundle

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ const { keyId } = require('@isomorphic-pgp/util/keyId')
 
 module.exports.pgp = {
   async sign ({ payload, secretKey }) {
-    payload = Buffer.from(payload)
     let signature = await ipgp.sign(secretKey, payload, Math.floor(Date.now() / 1000))
     return { signature }
   },
   async verify ({ payload, publicKey, signature }) {
-    payload = Buffer.from(payload)
     let sigValid = await ipgp.verify(publicKey, signature, payload)
     let invalid = []
     let valid = []
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
+const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin')
+
+module.exports = [
+  {
+    target: 'web',
+    entry: {
+      'isomorphic-git-pgp-plugin': './src/index.js'
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].min.js',
+      library: 'IsomorphicPgpPlugin',
+      libraryTarget: 'umd'
+    },
+    mode: 'production',
+    devtool: 'source-map',
+    plugins: [
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+        analyzerMode: 'static',
+        reportFilename: 'size_report.html',
+        defaultSizes: 'gzip'
+      }),
+      new DuplicatePackageCheckerPlugin({
+        strict: true
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          // exclude: /node_modules/,    // We actually DO want to transpile our dependencies
+          use: {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              plugins: [
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/plugin-transform-async-to-generator'
+              ]
+            }
+          }
+        }
+      ]
+    }
+  }
+]
